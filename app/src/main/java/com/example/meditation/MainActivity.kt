706ea82547f8dbec7f016818a5f0907a37451581
HossamSadekk <66652255+HossamSadekk@users.noreply.github.com>
2022-09-17 07:00:29 -0700

    Adding Bottom Menu

--- app/src/main/java/com/example/meditation/MainActivity.kt
+++ app/src/main/java/com/example/meditation/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.meditation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.meditation.ui.HomeScreen
+import com.plcoding.meditationuiyoutube.ui.theme.MeditationUITheme
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MeditationUITheme {
+                HomeScreen()
+            }
         }
     }
 }
--- app/src/main/java/com/example/meditation/ui/HomeScreen.kt
+++ app/src/main/java/com/example/meditation/ui/HomeScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.unit.sp
 import com.example.meditation.R
 import com.example.meditation.standardQuadFromTo
 import com.plcoding.meditationuiyoutube.ui.theme.*
+
 @Preview
 @Composable
 fun HomeScreen() {
@@ -73,6 +74,87 @@ fun HomeScreen() {
                 )
             )
         }
+        BottomMenu(items = listOf(
+            BottomMenuContent("Home", R.drawable.ic_home),
+            BottomMenuContent("Meditate", R.drawable.ic_bubble),
+            BottomMenuContent("Sleep", R.drawable.ic_moon),
+            BottomMenuContent("Music", R.drawable.ic_music),
+            BottomMenuContent("Profile", R.drawable.ic_profile),
+        ), modifier = Modifier.align(Alignment.BottomCenter))
+    }
+}
+
+@Composable
+fun BottomMenu(
+    items: List<BottomMenuContent>,
+    modifier: Modifier = Modifier,
+    activeHighlightColor: Color = ButtonBlue,
+    activeTextColor: Color = Color.White,
+    inactiveTextColor: Color = AquaBlue,
+    initialSelectedItemIndex: Int = 0
+) {
+    var selectedItemIndex by remember {
+        mutableStateOf(initialSelectedItemIndex)
+    }
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(DeepBlue)
+            .padding(15.dp)
+    ) {
+        items.forEachIndexed { index, item ->
+            BottomMenuItem(
+                item = item,
+                isSelected = index == selectedItemIndex,
+                activeHighlightColor = activeHighlightColor,
+                activeTextColor = activeTextColor,
+                inactiveTextColor = inactiveTextColor
+            ) {
+                selectedItemIndex = index
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomMenuItem(
+    item: BottomMenuContent,
+    isSelected: Boolean = false,
+    activeHighlightColor: Color = ButtonBlue,
+    activeTextColor: Color = Color.White,
+    inactiveTextColor: Color = AquaBlue,
+    onItemClick: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .clickable {
+                onItemClick()
+            }
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .background(if (isSelected) activeHighlightColor else Color.Transparent)
+                .padding(10.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = item.iconId),
+                contentDescription = item.title,
+                tint = if (isSelected) activeTextColor else inactiveTextColor,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+        Text(
+            text = item.title,
+            color = if (isSelected) activeTextColor else inactiveTextColor
+        )
+
     }
 }
 
@@ -208,20 +290,21 @@ fun FeatureItem(feature: FeatureUi) {
         val width = constraints.maxWidth
         val height = constraints.maxHeight
 
-        val mediumColorPoint1 = Offset(0f, height * 0.3f)
-        val mediumColorPoint2 = Offset(width * 0.1f, height * 0.35f)
-        val mediumColorPoint3 = Offset(width * 0.4f, height * 0.05f)
-        val mediumColorPoint4 = Offset(width * 0.075f, height * 0.7f)
-        val mediumColorPoint5 = Offset(width * 0.4f, -height.toFloat())
+        // Medium colored path
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
 
-        val mediumColorPath = Path().apply {
-            moveTo(mediumColorPoint1.x, mediumColorPoint1.y)
-            standardQuadFromTo(mediumColorPoint1, mediumColorPoint2)
-            standardQuadFromTo(mediumColorPoint2, mediumColorPoint3)
-            standardQuadFromTo(mediumColorPoint3, mediumColorPoint4)
-            standardQuadFromTo(mediumColorPoint4, mediumColorPoint5)
-            lineTo(width.toFloat() * 100f, height.toFloat() * 100f)
-            lineTo(-100f, height.toFloat() * 100f)
+        val mediumColoredPath = Path().apply {
+            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
+            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
+            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
+            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
             close()
         }
         // Light colored path
@@ -243,41 +326,42 @@ fun FeatureItem(feature: FeatureUi) {
         }
 
         Canvas(modifier = Modifier.fillMaxSize()) {
-            drawPath(path = mediumColorPath, color = feature.mediumColor)
+            drawPath(path = mediumColoredPath, color = feature.mediumColor)
             drawPath(path = lightColoredPath, color = feature.lightColor)
         }
-    }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(15.dp)
-    ) {
-        Text(
-            text = feature.title,
-            style = MaterialTheme.typography.h2,
-            lineHeight = 26.sp,
-            modifier = Modifier.align(Alignment.TopStart)
-        )
-        Icon(
-            painter = painterResource(id = feature.iconId),
-            contentDescription = feature.title,
-            tint = Color.White,
-            modifier = Modifier.align(Alignment.BottomStart)
-        )
-        Text(
-            text = "Start",
-            color = TextWhite,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Bold,
+        Box(
             modifier = Modifier
-                .clickable {
-                    // handle click
-                }
-                .align(Alignment.BottomEnd)
-                .clip(RoundedCornerShape(10.dp))
-                .background(ButtonBlue)
-                .padding(vertical = 6.dp, horizontal = 15.dp)
-        )
+                .fillMaxSize()
+                .padding(15.dp)
+        ) {
+            Text(
+                text = feature.title,
+                style = MaterialTheme.typography.h2,
+                lineHeight = 26.sp,
+                modifier = Modifier.align(Alignment.TopStart)
+            )
+            Icon(
+                painter = painterResource(id = feature.iconId),
+                contentDescription = feature.title,
+                tint = Color.White,
+                modifier = Modifier.align(Alignment.BottomStart)
+            )
+            Text(
+                text = "Start",
+                color = TextWhite,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .clickable {
+                        // handle click
+                    }
+                    .align(Alignment.BottomEnd)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(ButtonBlue)
+                    .padding(vertical = 6.dp, horizontal = 15.dp)
+            )
+        }
     }
+
 }
 
